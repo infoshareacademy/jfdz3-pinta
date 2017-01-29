b21ac4c7a335af5518dca4e09ee1d75fa984674d
Enea Jorgji <jorgji.enea@gmail.com>
2017-01-29 20:42:07 +0100

    JS, nagówka, navbar

--- js/script.js
+++ js/script.js
@@ -33,3 +33,29 @@ $(window).scroll(function() {
         $('#toTop').fadeOut();
     }
 });
+
+// changes page title, w nagowce
+
+$(function () {
+
+    var siteTitles = [];
+    var newTitle = ".swapp";
+
+    $('.navbar-nav a').each(function () {
+        siteTitles.push({
+            "position": $($(this).attr('href')).offset().top-150,
+            "title": $(this).attr('data-siteTitle')
+        });
+    });
+
+    $(window).scroll(function () {
+        siteTitles.forEach(function (siteTitle) {
+            if ($(window).scrollTop() > siteTitle.position) {
+                newTitle = siteTitle.title;
+            }
+        });
+
+        $('title').text(newTitle);
+
+    });
+});
